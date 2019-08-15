ee7d8748bde24fd3c02a049638ca5a749b8767d3
Roberto Martinez <roberto@sml.mx>
2019-08-14 18:50:10 -0600

    config db for postgresql

--- ferreteria_back/settings.py
+++ ferreteria_back/settings.py
@@ -122,8 +122,12 @@ AUTH_USER_MODEL = 'accounts.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT')
     }
 }
 
